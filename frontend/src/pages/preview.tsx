02054194ac1f39055d8e7dae8f56acece8c7dd55
I Made Cakra Pustaka <imadecakra20@gmail.com>
2026-05-13 22:03:18 +0800

    feat(fe): add camera toggle button

--- frontend/src/pages/preview.tsx
+++ frontend/src/pages/preview.tsx
@@ -1,8 +1,10 @@
+import { Camera, CameraOff } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
 export default function Preview() {
   const videoRef = useRef<HTMLVideoElement>(null);
   const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
+  const [webcamActive, setWebcamActive] = useState(false);
 
   const startWebcam = async () => {
     try {
@@ -13,9 +15,12 @@ export default function Preview() {
       if (videoRef.current) {
         videoRef.current.srcObject = stream;
       }
+
       setMediaStream(stream);
+      setWebcamActive(true);
     } catch (error) {
       console.log("Can't open camera", error);
+      setWebcamActive(false);
     }
   };
 
@@ -26,7 +31,17 @@ export default function Preview() {
     mediaStream.getTracks().forEach((track) => {
       track.stop();
     });
+
     setMediaStream(null);
+    setWebcamActive(false);
+  };
+
+  const toggleWebcam = () => {
+    if (!mediaStream) {
+      startWebcam();
+    } else {
+      stopWebcam();
+    }
   };
 
   useEffect(() => {
@@ -40,12 +55,20 @@ export default function Preview() {
           <h1 className="text-3xl sm:text-5xl font-bold text-gray-900 mb-2">Prepare to join room...</h1>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 sm:gap-6 w-full max-w-4xl">
-          <video id="webcam" autoPlay playsInline ref={videoRef} className="rounded-xl col-span-2"></video>
+          <div className="col-span-2 relative">
+            <video id="webcam" autoPlay playsInline ref={videoRef} className="w-full h-auto rounded-xl"></video>
+            <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex flex-row gap-2 items-center">
+              <button onClick={toggleWebcam} type="button" className="flex items-center justify-center rounded-2xl w-12 h-12 bg-blue-400/80 backdrop-blur-sm cursor-pointer hover:bg-blue-300">
+                {webcamActive? (<Camera/>) : (<CameraOff/>)}
+              </button>
+            </div>
+          </div>
           <div className="flex justify-center items-center">
-            <button type="button" className="cursor-pointer bg-indigo-300 max-h-10 w-[70%] text-white/90 rounded-md min-h-[5vh]">Join Room</button>
+            {/* <form action="" className="">
+              <button type="button" className="cursor-pointer bg-indigo-300 max-h-10 w-[70%] text-white/90 rounded-md min-h-[5vh]">Join Room</button>
+            </form> */}
           </div>
         </div>
-        {/* TODO: make a button turn off camera */}
       </div>
     </section>
   )
